5ffebe631244c60afd381360ce67948ecb2e19ac
ZackMisso <zack441@mac.com>
2014-10-18 17:29:20 -0500

    added the onTouchListener.

---
+++ SlingShot/app/src/main/java/kraz/slingshot/ShareableOnTouchListener.java
@@ -0,0 +1,16 @@
+package kraz.slingshot;
+
+/**
+ * Created by ZackMisso on 10/18/14.
+ */
+
+import android.view.View.OnTouchListener;
+import android.view.View;
+import android.view.MotionEvent;
+
+public class ShareableOnTouchListener implements OnTouchListener{
+    public boolean onTouch(View v,MotionEvent e){
+        // implement
+        return false;
+    }
+}
